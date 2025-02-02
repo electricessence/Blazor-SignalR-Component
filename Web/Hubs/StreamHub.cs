2389f5807538c2a85ef9cc7f97ab2bff11afd274
electricessence <5899455+electricessence@users.noreply.github.com>
2025-02-02 09:02:46 -0800

    Stable prep.

---
+++ Web/Hubs/StreamHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorWeb.Hubs;
+
+/// <summary>
+/// SignalR hub for subscribing to streams.
+/// </summary>
+public class StreamHub : Hub
+{
+	/// <summary>
+	/// Subscribe to a topic.
+	/// </summary>
+	public Task SubTo(string topic)
+		=> Groups.AddToGroupAsync(Context.ConnectionId, topic);
+
+	/// <summary>
+	/// Unsubscribe from a topic.
+	/// </summary>
+	/// <param name="topic"></param>
+	/// <returns></returns>
+	public Task UnsubFrom(string topic)
+		=> Groups.RemoveFromGroupAsync(Context.ConnectionId, topic);
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,7 @@
 using BlazorWeb.Client.Pages;
 using BlazorWeb.Components;
+using BlazorWeb.Hubs;
+using Microsoft.AspNetCore.SignalR;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,4 +34,27 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorWeb.Client._Imports).Assembly);
 
-app.Run();
+app.MapHub<StreamHub>("/hubs/stream");
+
+await app.StartAsync();
+
+var rnd = new Random();
+var streamHubContext = app.Services.GetRequiredService<IHubContext<StreamHub>>();
+using var cts = new CancellationTokenSource();
+var chaosProducer = Task
+    .Run(async () =>
+    {
+        while(!cts.Token.IsCancellationRequested)
+		{
+			await Task.Delay(500);
+			string message = rnd.Next(0, 1000).ToString();
+			await streamHubContext
+                .Clients.Group("chaos")
+				.SendAsync("OnNext", message);
+		}
+	}, cts.Token)
+    .ContinueWith(_ => { /* Ignore exeptions */});
+
+await app.WaitForShutdownAsync();
+cts.Cancel();
+await chaosProducer;
